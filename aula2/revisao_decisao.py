5080a3501523619c7500f0974944fbe8f2e75e06
kawanmoura <kawanlemesmouragmail.com>
2025-02-15 10:41:28 -0300

    add

--- aula2/revisao_decisao.py
+++ aula2/revisao_decisao.py
@@ -10,6 +10,25 @@ elif ceu == "Sol ao lado da terra":
 
 elif ceu == "Muita chuva":
     print("Saio com guarda-chuva")
-    
+
 else:
     print("Saio de boa")
+
+
+
+
+# Cinema
+# gasparzinho 3 -> 18 anos
+idade = 25
+
+if idade >= 18:
+    print("Pode assistir Gasparzinho 3")
+
+elif idade > 50:
+    print("Procure outro filme")
+
+elif idade < 18:
+    print(" Pode não fiot")
+
+else:
+    print("Pode assistir")
