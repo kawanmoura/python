00e3eac650626a2b0019b8f84590bcd5048cd772
kawanmoura <kawanlemesmouragmail.com>
2025-02-15 10:30:46 -0300

    revisão decisão

---
+++ aula2/revisao_decisao.py
@@ -0,0 +1,15 @@
+
+# IF -> SE
+ceu = "Fim do mundo"
+
+if ceu == "Fim do mundo":
+    print("Saio não")
+
+elif ceu == "Sol ao lado da terra":
+    print("Saio também não")
+
+elif ceu == "Muita chuva":
+    print("Saio com guarda-chuva")
+    
+else:
+    print("Saio de boa")
---
+++ aula2/revisão_math.py
@@ -0,0 +1,14 @@
+# +
+soma = 8 + 8
+# -
+subtracao = 8 - 8
+# *
+multiplicacao = 8 * 8
+# /
+divisao = 8 / 64
+
+# expoente
+expoente = 2 ** 3 #2³
+
+#resto
+resto = 10 % 3 # 1
