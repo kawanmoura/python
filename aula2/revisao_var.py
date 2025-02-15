7cfd70817202d0dbf05d34e00132ae54496839d2
kawanmoura <kawanlemesmoura@gmail.com>
2025-02-15 10:12:01 -0300

    subindo

---
+++ aula2/revisao_var.py
@@ -0,0 +1,37 @@
+# declarar uma variável
+
+# String
+nome = "Arthur"
+
+# Inteiro
+idade = 27
+
+# Boolean
+adulto = False
+
+# Float
+saldo = 3.14
+negativos = -2.5
+
+# Listas em Python
+listas = [True, "texto", 10, -50]
+lista = [1, 2, 3]
+# Vetor
+# A dicionar, editar, remover
+
+# Tuplas
+# Estrutura ordenada
+# Tupla é imutável
+cores = ("azul", "vermelho", "verde")
+
+# Dict
+pessoa = {
+    "nome": "Arthur",
+    "idade": 27,
+    "cidade": "São Paulo"
+}
+
+# set
+numeros = {1, 2, 3, 4}
+# set nao armazena numeros iguais somente unicos
+# numeors = {1, 2, 3, 4}
